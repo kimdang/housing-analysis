7265de50f268729781aa26e20baf216cd395ca87
Kim Dang <nkdang0795@gmail.com>
2020-06-23 22:53:53 -0700

    add template and form

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path 
 from . import views 
 
+app_name = 'myapp'
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:question_id>', views.detail, name='details'),
-    path('<int:question_id>/results/', views.result, name='results')
+    path('<int:question_id>', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.result, name='result'),
+    path('comment', views.comment, name='comment')
+    # the name value is called by {% url %} template tag in index.html 
 ]
 
 """
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse 
+from django.http import Http404
 
 from django.template import loader
 
@@ -12,7 +13,7 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
     """
-    if you do this, you don't have to import loader
+    if you do this, you don't have to import loader, see detail() below as example 
     def index (request):
         ... 
         question_list = Question.objects.all()
@@ -25,14 +26,25 @@ def index(request):
     # render(request object, template name, optional dictionary)
     # render() returns HttpResponse object of given template rendered with given context
 
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'myapp/detail.html', {'question':question})
+
+"""
+how to raise 404 error without using get_object_or_404()
 def detail (request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
-        raise Http404("Question does not exist :(")
+        raise Http404("Question does not exist")
     return render(request, 'myapp/detail.html', {'question': question})
+"""
 
 def result(request, question_id):
-    return HttpResponse("You're looking at the result of %s question." %question_id) 
+    return HttpResponse("You're looking at the result of question %s." %question_id)
+
+def comment(request):
+    # need content here
+    return HttpResponse("Ngan was here")
 
 
