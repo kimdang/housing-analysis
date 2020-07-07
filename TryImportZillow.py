6dda644e2dfe753583ab90ed242f5e83f1c54e1c
Kim Dang <nkdang0795@gmail.com>
2020-07-06 21:46:04 -0700

    import only CA data into sqlite database

--- TryImportZillow.py
+++ TryImportZillow.py
@@ -3,23 +3,33 @@ import pandas as pd
 
 data = pd.read_csv("ZillowByCity.csv")
 cityCount = data["RegionName"].count()
-dateEntry = len(data.columns)-8  # First 8 columns are not home prices.
+entryCount = len(data.columns)-8  # First 8 columns are not home prices.
 
-print(f"There are {cityCount} cities in dataset. Each city has {dateEntry} entries.")
-
-target = "San Diego"
-
-target_index = data.index[data['RegionName']==target]
-
-# subData = data.iloc([8])
-
-print(data.head(5))
-# conn = sqlite3.connect('db.sqlite3')
-# c = conn.cursor()
-
-# def create_table():
-#     c.execute('CREATE TABLE IF NOT EXISTS test_table (value REAL)')
+print(f"There are {cityCount} cities in dataset. Each city has {entryCount} entries.\n")
 
 
-# create_table()
-# print('Table created')
+subdata = data[data.StateName == "CA"]
+
+regionID = subdata['RegionID'].tolist()
+regionName = subdata['RegionName'].tolist()
+regionState = subdata['StateName'].tolist()
+
+
+
+conn = sqlite3.connect('db.sqlite3')
+c = conn.cursor()
+
+for i in range(len(regionID)):
+    text = "(%s, '%s', '%s')" %(regionID[i], regionName[i], regionState[i])
+    query = "INSERT INTO myapp_indexTable (regionID, regionName, regionState) VALUES %s" %(text)
+    print(query)
+    c.execute(query)
+
+
+
+
+print('Data inserted.')
+
+
+
+
--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 
+from .models import indexTable
+
+admin.site.register(indexTable)
