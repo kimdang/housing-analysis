41ad868411b1f0108dca480838611871e2ed20ff
Lambert Tran <lb.tran648@gmail.com>
2020-09-26 00:21:56 -0700

    fix myconfig path

--- bin/controller.sh
+++ bin/controller.sh
@@ -5,7 +5,7 @@ rootDir="${scriptDir}/../"
 
 start() {
     consul-template -consul-addr ${CONSUL_SERVER_ADDR}:8500 \
-                    -template "${rootDir}/consul/config.tpl:${rootDir}/myconfig.ini" \
+                    -template "${rootDir}/consul/config.tpl:${rootDir}/housingprice/myconfig.ini" \
                     -exec "python3 ${rootDir}/manage.py runserver 0.0.0.0:8000"
 }
 
