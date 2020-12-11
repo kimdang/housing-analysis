3969ae8efefc21fc55ef7ba2df5e2baa954dc2f3
Kim Dang <nkdang0795@gmail.com>
2020-12-11 00:00:23 -0800

    dictionary can now be saved and loaded (no need to re-run class every time the file is ran), create tables in SQL database for each city

--- main_data_processing.py
+++ main_data_processing.py
@@ -2,13 +2,36 @@
 from datatools import DataSet
 import SQLtools
 import execute
+import pickle
 
-example = DataSet('TopTierByCity.csv', 'toptier')
-example.sanitize(before2000=False)
-example.split()
-example.multi(process_no=5)
-print(example)
-example.dfprocessed.to_pickle("toptiertemp.pkl")
+# example = DataSet('TopTierByCity.csv', 'toptier')
+# example.sanitize(before2000=False)
+# example.split()
+# example.multi(process_no=5)
+# print(example)
+
+# def save_obj(obj, name):
+#     filename = open(name, "wb")
+#     pickle.dump(obj, filename)
+    
+# save_obj(example.dfprocessed, 'toptieroutput')
+
+
+def load_obj(name):
+    filename = open(name, "rb")
+    return(pickle.load(filename))
+
+savedfile = load_obj("toptieroutput")
+print(savedfile)
+
+
+
+
+for state in savedfile:
+    tablequery = "CREATE TABLE IF NOT EXISTS %s_toptier (price INT, date DATETIME NOT NULL, regionid INT, FOREIGN KEY (regionid) REFERENCES index_table(regionid))" %(state)
+    execute.run_query(tablequery)
+    print("%s is finished" %(state))
+
 
 '''
 CREATE INDEX TABLE
