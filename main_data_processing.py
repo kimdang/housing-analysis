48c91f3afc0d029ce9022146db8435889a96e695
Kim Dang <nkdang0795@gmail.com>
2020-12-21 23:34:20 -0800

    bug found: need to import all index tables and union them to create 1 main index tables

--- main_data_processing.py
+++ main_data_processing.py
@@ -5,7 +5,7 @@ import pickle
 
 
 
-######## FILL OUT THE 2 LISTS BELOW, MAKE SURE ITEMS ARE IN CORRECT ORDER ########
+######## FILL OUT THE 2 LISTS BELOW, MAKE SURE ITEMS ARE IN CORRECT ORDER! ########
 csvfile = ['TopTierByCity.csv', 'MidTierByCity.csv', 'BottomTierByCity.csv', 'OneBedByCity.csv', 'TwoBedByCity.csv', 'ThreeBedByCity.csv', 'FourBedByCity.csv', 'FiveBedByCity.csv']
 csvname = ['toptier', 'midtier', 'bottomtier', 'onebed', 'twobed', 'threebed', 'fourbed', 'fivebed']
 
@@ -22,26 +22,26 @@ def load_obj(name):
 
 
 
-print("Do you need to create index table? (yes/no)")
+print("Have you created index tables? (yes/no)")
 createIndexTable = input()
 
-print("Do you need to process all raw data? (yes/no)")
+if createIndexTable == "yes":
+    toptier = DataSet('TopTierByCity.csv', 'toptier') # use midtier's index DataFrame to create SQL index_table
+    toptier.clean(before2000=False)
+    text = SQLtools.dftostring(toptier.indexdf, key='index')
+    createQuery = "CREATE TABLE IF NOT EXISTS toptier_index (regionid INT PRIMARY KEY, cityname VARCHAR(255), statename VARCHAR(255))"
+    execute.run_query(createQuery)
+    insertQuery = "INSERT INTO toptier_index (regionid, cityname, statename) VALUES %s" %(text)
+    execute.run_query(insertQuery)
+    print('Tables are created in MySQL database.')
+
+
+
+print("Do you want to process raw data? (yes/no)")
 processRawData = input()
 
-
-if createIndexTable == "yes":
-    midtier = DataSet('MidTierByCity.csv', 'midtier') # use midtier's index DataFrame to create SQL index_table
-    midtier.clean(before2000=False)
-    text = SQLtools.dftostring(midtier.indexdf, key='index')
-    createQuery = "CREATE TABLE IF NOT EXISTS index_table (regionid INT PRIMARY KEY, cityname VARCHAR(255), statename VARCHAR(255))"
-    execute.run_query(createQuery)
-    insertQuery = "INSERT INTO index_table (regionid, cityname, statename) VALUES %s" %(text)
-    execute.run_query(insertQuery)
-    print('index_table is created in SQL database.')
-    
-
 if processRawData == 'yes':
-# This code only need to be executed once.
+# This script only needs to be executed once.
     for filename, name in zip(csvfile, csvname):
         print("working on " + name + "...")
         tempobj = DataSet(filename, name)
@@ -53,17 +53,20 @@ if processRawData == 'yes':
 
 
 
+print("Do you want to send data to SQL database? (yes/no)")
+sendToDatabase = input()
 
-
-
-
-
-# for state in savedfile:
-#     createTableQuery = "CREATE TABLE IF NOT EXISTS %s_toptier (price INT, date DATETIME NOT NULL, regionid INT, FOREIGN KEY (regionid) REFERENCES index_table(regionid))" %(state)
-#     execute.run_query(createTableQuery)
-#     stateText = SQLtools.dftostring(savedfile[state], key='state')
-#     insertQuery = "INSERT INTO %s_toptier (price, date, regionid) VALUES %s" %(state, stateText)
-#     execute.run_query(insertQuery)
-#     print("%s is finished" %(state))
+if sendToDatabase == 'yes':    
+    for item in csvname:
+        nameOfDict = item + "output"
+        tempDict = load_obj(nameOfDict)
+        for state in tempDict:
+            createTableQuery = "CREATE TABLE IF NOT EXISTS %s_%s (price INT, date DATETIME NOT NULL, regionid INT, FOREIGN KEY (regionid) REFERENCES index_table(regionid))" %(state, item)
+            execute.run_query(createTableQuery)
+            stateText = SQLtools.dftostring(tempDict[state], key='state')
+            insertQuery = "INSERT INTO %s_%s (price, date, regionid) VALUES %s" %(state, item, stateText)
+            print(insertQuery)
+            execute.run_query(insertQuery)
+            print("%s_%s is now in MySQL database." %(state, item))
 
 
